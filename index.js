9046ff42577b7849ce8ae1c59372269588913275
Diana Minenko <kundiiy@icloud.com>
2025-10-14 20:25:52 +0200

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+    return wrapper
+}
+
+
+const btn = document.querySelector('.btn-outline-primary');
+let currentAlert = null;
+let count = 0
+
+
+btn.addEventListener('click', (event) => {
+    count++
+    if(count % 2 === 1) {
+        currentAlert = appendAlert('Nice, you triggered this alert message!', 'success')
+    } else{
+        currentAlert.classList.add('hidden')
+    }
+})
+
